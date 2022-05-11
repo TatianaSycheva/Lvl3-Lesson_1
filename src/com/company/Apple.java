31568a785a58609cb5ed22d3ab2858c21a979b0e
SychevaTatiana <svchevatan06119989@gmail.com>
2022-05-11 16:36:55 +0400

    Lesson_1

---
+++ src/com/company/Apple.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Apple extends Fruit {
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString() {
+        return "Яблоки";
+    }
+
+
+}
